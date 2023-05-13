79801607105b67b75ec156db86c745446c083664
howard <zhihao.zhou.bupt@gmail.com>
2023-05-13 19:20:18 +0800

    add new int64, int8, uint32, int32, float64, bool

--- new.go
+++ new.go
@@ -7,3 +7,27 @@ func NewString(s string) *string {
 func NewUint64(i uint64) *uint64 {
 	return &i
 }
+
+func NewInt64(i int64) *int64 {
+	return &i
+}
+
+func NewInt8(i int8) *int8 {
+	return &i
+}
+
+func NewUint32(i uint32) *uint32 {
+	return &i
+}
+
+func NewInt32(i int32) *int32 {
+	return &i
+}
+
+func NewFloat64(f float64) *float64 {
+	return &f
+}
+
+func NewBool(b bool) *bool {
+	return &b
+}
